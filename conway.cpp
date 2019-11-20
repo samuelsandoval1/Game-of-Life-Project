09bb886bc75371c069cb227a23f7140386adbaad
sjsandoval <sjsandoval@cs3004a.acad.fullerton.edu>
2019-11-19 17:47:24 -0800

    Work

--- conway.cpp
+++ conway.cpp
@@ -1,6 +1,9 @@
 #include <chrono>
 #include <ctime>
 #include <curses.h>
+#include <chrono>
+#include <ctime>
+#include <curses.h>
 #include <iostream>
 #include <stdlib.h>
 #include <thread>
@@ -8,11 +11,12 @@ using namespace std;
 
 const int ROWS = 48;
 const int COLUMNS = 160;
+const char ALIVE = '*';
+const char DEAD = ' ';
+
 
 
 void InitializeCurses() {
-  // Initialize curses to control where to output to the terminal. No need to
-  // modify this function, but make sure it is called in main at the beginning.
   initscr();
   cbreak();
   noecho();
@@ -21,16 +25,19 @@ void InitializeCurses() {
 
 
 void PrintExitInstructions() {
-  // Prints instructions near the bottom of the screen for how to exit. No need
-  // to modify this function.
   mvaddstr(ROWS + 1, 0, "Press ctrl+c to quit ");
   refresh();
 }
 
+// void InitialState(){
+//
+// }
 
 void PrintRow(string row_to_print, int row) {
   mvaddstr(row, 0, row_to_print.c_str());
   refresh();
+  int neighbors = 0;
+
 
   // Optional: Use the following line to create a short delay between each
   // tick if you want to watch your population grow/shrink more slowly:
@@ -41,10 +48,13 @@ void PrintRow(string row_to_print, int row) {
 int main(int argc, char* argv[]) {
   InitializeCurses();
   PrintExitInstructions();
+  int current_gen[ROWS][COLUMNS];
+  int next_gen[ROWS][COLUMNS];
+
 
   // TODO: Implement Conway's Game of Life.
   while (true) {
-    
+
   }
 
   endwin();
